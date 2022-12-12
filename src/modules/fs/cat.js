ee3910f1fca0134d2aa6c6eb8d9c77fed5d8263f
Sergey <s.zaharevich@itransition.com>
2022-12-12 16:26:06 +0300

    fix: update ls, cat implementation modules

--- src/modules/fs/cat.js
+++ src/modules/fs/cat.js
@@ -1,15 +1,28 @@
 import {pipeline} from "stream/promises";
+import {Writable} from 'stream'
 import {createReadStream} from "fs";
 import {getPath} from "../../helpers/fileSystemHelper.js";
 import {printOperationFailedMessage} from "../../helpers/messageHelper.js";
+
+const stdout = () => {
+  const options = {
+    decodeStrings: false,
+    write(chunk, _, callback) {
+      console.log(chunk);
+      callback();
+    },
+  }
+
+  return new Writable(options);
+}
 
 const cat = async ([inputFilePath]) => {
   const filePath = getPath(inputFilePath);
 
   try {
     await pipeline(
-      createReadStream(filePath),
-      process.stdin
+      createReadStream(filePath, {encoding: 'UTF-8'}),
+      stdout(),
     );
   } catch (e) {
     printOperationFailedMessage();
--- src/modules/fs/ls.js
+++ src/modules/fs/ls.js
@@ -2,12 +2,27 @@ import {readdir} from 'fs/promises';
 import {getPath} from "../../helpers/fileSystemHelper.js";
 import {printOperationFailedMessage} from "../../helpers/messageHelper.js";
 
+const fileHandler = (item) => {
+  return {
+    name: item.name,
+    type: item.isFile() ? 'file' : 'directory',
+  };
+}
+
+const sortHandler = (itemA, itemB) => {
+  return itemA.type > itemB.type ? 1 : -1;
+}
+
 const ls = async ([inputDirPath]) => {
   const dirPath = getPath(inputDirPath);
 
   try {
-    const contentsList = await readdir(dirPath);
-    console.table(contentsList);
+    const contentsList = await readdir(dirPath, {withFileTypes: true});
+    const result = contentsList
+      .map(item => fileHandler(item))
+      .sort((itemA, itemB) => sortHandler(itemA, itemB));
+
+    console.table(result);
   } catch (e) {
     printOperationFailedMessage();
   }
